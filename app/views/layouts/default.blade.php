63622223f0fbdfd6891d9105b83920637c57ca69
kelvin123456789 <dmaina57@gmail.com>
2017-03-07 07:52:27 +0300

    video six done

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>Larabook</title>
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/main.css">
 </head>
 <body>
 @include('layouts.partials.nav')
