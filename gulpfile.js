c32d97c0ad15b27dbf53b0907efb4ea6cabe436e
kelvin123456789 <dmaina57@gmail.com>
2017-03-07 07:27:05 +0300

    additions to the project

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,13 @@ var sass= require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('css', function () {
-gulp.src('app/assets/sass/main.sass')
+gulp.src('app/assets/sass/main.scss')
     .pipe(sass())
     .pipe(gulp.dest('public/css'));
 
 });
+gulp.task('watch', function () {
+    gulp.watch('app/assets/sass/**/*.scss',['css']);
+
+});
+gulp.task('default',['watch']);
